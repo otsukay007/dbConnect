551b0bd70c6595e1c3a665342e2c3802dfb81a8c
otsukay007 <otsukay@iiujp.com>
2025-06-22 09:21:51 +0900

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -1,8 +1,55 @@
 # Database access Web Api
 # KVM claud mariadb
 # Author: iiujapan
-# Date: 205-6-15
+# Date: 2025-6-15
 # Version: 1.0.0
+
+# Define class
+class dbConnectClass {
+    [string]$ConnectionString
+    [System.Data.Odbc.OdbcConnection] $connection
+    [System.Data.Odbc.OdbcCommand] $command
+    [System.Data.Odbc.OdbcDataReader] $reader
+    [System.Data.Odbc.OdbcDataAdapter] $adapter
+    [System.Data.DataTable] $dataTable
+    [System.Data.Odbc.OdbcTransaction] $transaction
+    [string]$TableName
+    [string]$DatabaseName
+
+    dbConnectClass([string]$connectionString) {
+        $this.ConnectionString = $connectionString
+
+        $connectString = "DSN=KVM; UID=iiujapan; PWD=iiujapan;"
+        $this.connection = New-Object System.Data.Odbc.OdbcConnection($connectString)
+        $this.connection.Open()
+
+    }
+
+    [void]Connect() {
+        # Implement connection logic here
+        $this.command = $this.connection.CreateCommand( )
+        $this.command.CommandText = "select * from tmdb_info_Avatar2"
+    }
+
+    [void]ReadData() {
+        # Implement read logic here
+    }
+
+    [void]WriteData() {
+        # Implement write logic here
+    }
+
+    [void]Terminate() {
+        # Implement termination logic here 
+        if ($this.reader -ne $null) {
+            $this.reader.Close()
+        }
+        $this.command.Dispose()
+
+        $this.connection.Close()
+        $this.connection.Dispose()
+    }
+}
 
 Set-Location $PSScriptRoot
 
